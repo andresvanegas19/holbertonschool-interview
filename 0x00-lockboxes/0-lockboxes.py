0ea62c1307cd1d9257acbe918bfbf3c9127e6c9c
andres santiago reyes <andresdev@MacBook-Pro.local>
2021-10-02 01:12:04 -0500

    refactoring the boxing and put into the main array

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,41 +7,52 @@ def canUnlockAll(boxes):
     opened, else return  false couse the boxes will not opened'''
 
     # check if there is empty boxes
-    if len(boxes) == 1 and len(boxes[0]) == 0:
-        return True
+    # if len(boxes) == 1 and len(boxes[0]) == 0:
+    #     return True
 
-    validate = True
-    i = 0
-    end = total_len = len(boxes)
-    counter = 0
-    while boxes:
-        if not len(boxes[i]) and counter == 0:
-            validate = True
-            counter += 1
-            del boxes[i]
-            end -= 1
-            i -= 1
+    # validate = True
+    # i = 0
+    # end = total_len = len(boxes)
+    # counter = 0
+    # while boxes:
+    #     if not len(boxes[i]) and counter == 0:
+    #         validate = True
+    #         counter += 1
+    #         del boxes[i]
+    #         end -= 1
+    #         i -= 1
+    box_principal = [0]
 
-        for key in boxes[i]:
-            if counter == key:
-                counter += 1
-                validate = True
-                del boxes[i]
-                end -= 1
-                i -= 1
-                break
+    for key in box_principal:
+        for keyBox in boxes[key]:
+            if keyBox not in box_principal:
+                if keyBox < len(boxes):
+                    box_principal.append(keyBox)
 
-        if (not validate and i == end - 1) or (total_len == counter):
-            break
-
-        # means reach the end of the boxes
-        if end - 1 == i:
-            validate = False
-            i = -1
-
-        i += 1
-
-    if counter == total_len and len(boxes) == 0:
+    if len(box_principal) == len(boxes):
         return True
 
     return False
+    #     for key in boxes[i]:
+    #         if counter == key:
+    #             counter += 1
+    #             validate = True
+    #             del boxes[i]
+    #             end -= 1
+    #             i -= 1
+    #             break
+
+    #     if (not validate and i == end - 1) or (total_len == counter):
+    #         break
+
+    #     # means reach the end of the boxes
+    #     if end - 1 == i:
+    #         validate = False
+    #         i = -1
+
+    #     i += 1
+
+    # if counter == total_len and len(boxes) == 0:
+    #     return True
+
+    # return False
