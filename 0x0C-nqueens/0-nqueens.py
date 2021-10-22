fac8468c79843a2cba056ab37058391110432eef
andres reyes <andressantiagore@gmail.com>
2021-10-21 21:00:36 -0500

    change to the old logic and add print

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -4,7 +4,6 @@
 The N queens puzzle is the challenge of placing N
 non-attacking queens on an N×N chessboard. Write a
 program that solves the N queens problem.
-
 sage: nqueens N
 If the user called the program with the wrong number
 of arguments, print Usage: nqueens N, followed by a new
@@ -12,7 +11,6 @@ line, and exit with the status 1
     where N must be an integer greater or equal to 4
     - If N is not an integer, print N must be a number, followed
     by a new line, and exit with the status 1
-
     - If N is smaller than 4, print N must be at least 4,
     followed by a new line, and exit with the status 1
 """
@@ -20,65 +18,72 @@ line, and exit with the status 1
 from sys import argv
 
 
-def validate_table(solve, temp):
+def validate_table(large):
     """
     Validate the table
-
     Args:
         cell (list): [description]
-
     Returns:
         [type]: [description]
     """
-    for row in solve:
-        if row[1] == temp[1]:
-            return False
+    l_R = len(large) - 1
 
-        if (row[0] + row[1]) == (temp[0] + temp[1]):
-            return False
+    difference = 0
 
-        if (row[0] - row[1]) == (temp[0] - temp[1]):
+    for i in range(l_R):
+        difference = large[i] - large[l_R]
+
+        if difference < 0:
+            difference *= -1
+
+        if difference == 0 or difference == l_R - i:
             return False
 
     return True
 
 
-def resolve_quens(income, row, solve):
+def resolve_quens(dim, row, large, outcome):
     """
     resolve the code
-
     Args:
         dim (int): [description]
         row (int): [description]
         large (list): [description]
         outcome (list): [description]
     """
-    if (row == income):
-        print(solve)
+    if row == dim:
+        print(outcome)
 
     else:
-        for col in range(income):
-            temp = [row, col]
+        for col in range(0, dim):
+            large.append(col)
+            outcome.append([row, col])
+            cond = validate_table(large)
 
-            if validate_table(solve, temp):
-                solve.append(temp)
-                resolve_quens(row + 1, income, solve)
-                solve.remove(temp)
+            if (cond):
+                resolve_quens(dim, row + 1, large, outcome)
+
+            large.pop()
+            outcome.pop()
 
 
 if __name__ == "__main__":
     if len(argv) != 2:  # validate not more args
+        print('Usage: nqueens N')
         exit(1)
 
     try:
         N = int(argv[1])
     except BaseException:  # Validate the number
+        print('N must be a number')
         exit(1)
     if N < 4:  # be must be 4
+        print('N must be at least 4')
         exit(1)
     else:
         resolve_quens(
             N,
             0,
             [],
+            []
         )
