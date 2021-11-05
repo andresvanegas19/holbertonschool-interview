08d49c1f6dd59805589f27517fad4c76bdcb18cc
andres santiago reyes <andresdev@MacBook-Pro.local>
2021-11-04 19:34:42 -0500

    fix the betty Warning the lines

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -10,43 +10,43 @@
 */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *pnode;
+	skiplist_t *pnode;
 
-    if (!list)
-        return (NULL);
+	if (!list)
+		return (NULL);
 
-    pnode = list->express;
+	pnode = list->express;
 
-    printf("Value checked at index [%lu] = [%d]\n", pnode->index, pnode->n);
+	printf("Value checked at index [%lu] = [%d]\n", pnode->index, pnode->n);
 
-    while (pnode && pnode->n < value)
-    {
-        list = pnode;
-        pnode = pnode->express;
-        if (!pnode)
-        {
-            pnode = list;
+	while (pnode && pnode->n < value)
+	{
+		list = pnode;
+		pnode = pnode->express;
+		if (!pnode)
+		{
+			pnode = list;
 
-            while (pnode->next)
-                pnode = pnode->next;
-            break;
-        }
+			while (pnode->next)
+				pnode = pnode->next;
+			break;
+		}
 
-        printf("Value checked at index [%lu] = [%d]\n", pnode->index, pnode->n);
-    }
+		printf("Value checked at index [%lu] = [%d]\n", pnode->index, pnode->n);
+	}
 
-    printf(
-        "Value found between indexes [%lu] and [%lu]\n",
-        list->index,
-        pnode->index);
+	printf(
+	    "Value found between indexes [%lu] and [%lu]\n",
+	    list->index,
+	    pnode->index);
 
-    for (; list && pnode->n >= list->n; list = list->next)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+	for (; list && pnode->n >= list->n; list = list->next)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
 
-        if (list->n == value)
-            return (list);
-    }
+		if (list->n == value)
+			return (list);
+	}
 
-    return (NULL);
+	return (NULL);
 }
