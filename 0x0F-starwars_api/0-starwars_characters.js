2858698c956cea153fa20dff4867a2db4da21eba
andres santiago reyes <andresdev@MacBook-Pro.local>
2021-11-11 18:36:51 -0500

    insert into the void

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,33 +1,29 @@
 #!/usr/bin/node
 
 const request = require("request");
+const baseURL = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 void (async function () {
-  try {
-    const movieId = process.argv[2];
-    const url = `https://swapi-api.hbtn.io/api/${movieId}/`;
+  request(baseURL, async function (error, response, body) {
+    if (error) {
+      console.log(error);
+      return;
+    }
 
-    request(url, async function (error, response, body) {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      const characters = JSON.parse(body).characters;
-      for (const character of characters) {
-        const res = await new Promise((resolve, reject) => {
-          request(character, (err, res, html) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(JSON.parse(html).name);
-            }
-          });
+    const characters = JSON.parse(body).characters;
+
+    for (const character of characters) {
+      await new Promise(function (resolve, reject) {
+        request(character, function (err, res, bod) {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            console.log(JSON.parse(bod).name);
+            resolve();
+          }
         });
-
-        console.log(res);
-      }
-    });
-  } catch (catchErr) {
-    console.log(catchErr);
-  }
+      });
+    }
+  });
 })();
