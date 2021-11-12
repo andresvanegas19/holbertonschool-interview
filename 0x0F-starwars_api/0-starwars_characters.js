8808733ee3fb59c1c325bb5c189bfa3fcf8db34d
andres santiago reyes <andresdev@MacBook-Pro.local>
2021-11-12 12:55:19 -0500

    update

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 const baseURL = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
-void (async function () {
+(async function () {
   request(baseURL, async function (error, response, body) {
     if (error) {
       console.log(error);
