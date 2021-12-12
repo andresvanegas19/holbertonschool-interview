ef4168ed3c9b59cd7fa6e86b0ef96e6718fc0cd5
andres santiago reyes <andresdev@MacBook-Pro.local>
2021-12-11 21:59:51 -0500

    modify to one function

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -2,54 +2,10 @@
 """ Count it! """
 import requests
 
-URL_TEMPLATE = 'https://www.reddit.com/r/{}/hot.json?after={}'
+REDDIT_URL = 'https://reddit.com/r/{}/hot.json'
 
 
-def finding(subreddit, word_list, most_s_titles, next_s_redd=""):
-    """
-    Query reddit for hot posts and print total occurrences of each keyword in
-    a recursive way
-
-    Args:
-        subreddit(string): string representing subreddit to search for
-        word_list(array): keywords to search in the subreddit
-        most_s_titles: dictionary of number of occurrences of elements
-        in word_list that are found in the request
-        next_s_redd: pagination feature of the reddit api
-
-    Return:
-        most_s_titles or None
-    """
-
-    # Making the request
-    res = requests.get(
-        # the url
-        "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, next_s_redd),
-        headers={'User-agent': 'product'},
-        allow_redirects=False
-    )
-
-    if res.status_code != 200:
-        return None
-    if next_s_redd is None:
-        return most_s_titles
-
-    for i in res.json().get('data').get('children'):
-        title_s = i.get('data').get('title').split()
-        for word in set(word_list):
-            if word.lower() in [x.lower() for x in title_s]:
-                if most_s_titles.get(word):
-                    most_s_titles[word] += 1
-                else:
-                    most_s_titles[word] = 1
-
-    next_s_redd = res.json().get('data').get('after')
-    finding(subreddit, word_list, most_s_titles, next_s_redd)
-    return most_s_titles
-
-
-
-def count_words(subreddit, word_list):
+def count_words(subreddit, word_list, mfsr=None, init_key_val={}, res_dic={}):
     """
     Query reddit for hot posts and print total occurrences of each keyword in
     a recursive way
@@ -67,17 +23,56 @@ def count_words(subreddit, word_list):
     Return:
         each keyword with its corresponding count in order
     """
-    most_s_title = finding(subreddit, word_list, {})
+    pagination = ''
+    if mfsr:
+        pagination += '?after=' + mfsr
 
-    if most_s_title:
-        f_sorted = sorted(
-            # sort by value
-            most_s_title.items(),
-            # lambda function to sort by key
-            key=lambda val: val[1],
-            # reverse order
-            reverse=True
+    response = requests.get(
+        REDDIT_URL.format(subreddit) + pagination,
+        headers={"User-Agent": "client"}
+    )
+
+    if response.status_code != 200:
+        return
+
+    if init_key_val == {}:
+        # Initialize the dictionary
+        keys = [word.lower() for word in word_list]
+        # Initialize the dictionary with 0
+        for key in keys:
+            init_key_val[key] = 0
+            res_dic[key] = keys.count(key)
+
+    response_dict = response.json()
+    # Get the data from the response
+    data = response_dict['data']
+    mfsr = data['after']
+    posts = data['children']
+
+    for post in posts:
+        # Get the title of the post
+        data_in_post = post['data']
+        title = data_in_post['title']
+        words_in_title = title.split()
+        # Check if the word is in the title
+        for word in words_in_title:
+            for key in init_key_val:
+                if word.lower() == key.lower():
+                    init_key_val[key] += 1
+
+    if mfsr:
+        # Call the function again
+        count_words(subreddit, word_list, mfsr, init_key_val, res_dic)
+    else:
+        for key, val in res_dic.items():
+            if val > 1:
+                init_key_val[key] *= val
+
+        s_resp = sorted(
+            sorted(init_key_val.items(), key=lambda x: x[0]),
+            key=lambda x: (-x[1], x[0])
         )
-        for k, v in f_sorted:
-            if v != 0:
-                print('{}: {}'.format(k, v))
+
+        for res in s_resp:
+            if res[1] > 0:
+                print('{}: {}'.format(res[0], res[1]))
