b4ceba31e96372d1e1e658826cfe982964283fcf
andresvanegas19 <andressantiagore@gmail.com>
2022-01-12 11:27:02 -0500

    fix the indentation

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -9,26 +9,26 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *confim = head;
-    listint_t *p_next_head = head;
+	listint_t *confim = head;
+	listint_t *p_next_head = head;
 
-    if (!head)
-        return (NULL);
+	if (!head)
+		return (NULL);
 
-    while (p_next_head && p_next_head->next)
-    {
-        confim = confim->next;
-        p_next_head = p_next_head->next->next;
-        if (confim == p_next_head)
-        {
-            confim = head;
-            while (confim != p_next_head)
-            {
-                confim = confim->next;
-                p_next_head = p_next_head->next;
-            }
-            return (p_next_head);
-        }
-    }
-    return (NULL);
+	while (p_next_head && p_next_head->next)
+	{
+		confim = confim->next;
+		p_next_head = p_next_head->next->next;
+		if (confim == p_next_head)
+		{
+			confim = head;
+			while (confim != p_next_head)
+			{
+				confim = confim->next;
+				p_next_head = p_next_head->next;
+			}
+			return (p_next_head);
+		}
+	}
+	return (NULL);
 }
