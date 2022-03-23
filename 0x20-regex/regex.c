2dbc11de79f90059906dc6d8d1d952540d540b41
“andresvanegas19” <andressantiagore@gmail.com>
2022-03-23 12:45:16 -0500

    solving the code indetation

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -1,29 +1,29 @@
 #include "regex.h"
 
 /**
-* regex_match - checks whether a given pattern matches a given string
-* @str: is the string to scan, can be empty, and can contain any ASCII
-* character in the exception of . and *
-* @pattern: is the regular expression's pattern, can be empty, and
-* can contain any ASCII character, including . and *
-*
-* Note:
-* The regular expression matching must support the characters . and *
-* matches any single character matches zero or more of the preceding character
-*
-* Return: return 1 if true, 0 if not
-*/
+ * regex_match - checks whether a given pattern matches a given string
+ * @str: is the string to scan, can be empty, and can contain any ASCII
+ * character in the exception of . and *
+ * @pattern: is the regular expression's pattern, can be empty, and
+ * can contain any ASCII character, including . and *
+ *
+ * Note:
+ * The regular expression matching must support the characters . and *
+ * matches any single character matches zero or more of the preceding character
+ *
+ * Return: return 1 if true, 0 if not
+ */
 int regex_match(char const *str, char const *pattern)
 {
 	if (!str)
 		return (0);
 
-    /* str checking the nulling */
+	/* str checking the nulling */
 	if (!*str)
 	{
-        /* checking the pattern  */
+		/* checking the pattern  */
 		if (*(pattern + 1) == '*')
-            /* validating recursion */
+			/* validating recursion */
 			return (regex_match(str, pattern + 2));
 
 		return (!*pattern);
